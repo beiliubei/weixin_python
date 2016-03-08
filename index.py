c54283f8947c048d800df591b42fc7994565d237
Liu Zihao <sniperlzh@foxmail.com>
2016-03-08 14:26:54 +0800

    test

--- index.py
+++ index.py
@@ -168,8 +168,8 @@ class index:
 		if text == 'test':
 			msg = 'test too！'
 		elif text == 'db':
-			db = web.database(dbn='mysql',db='mysql',host='180.165.181.226',port='8306',user='root',pw='')
-			msg = db.select('user')
+			#db = web.database(dbn='mysql',db='mysql',host='180.165.181.226',port='8306',user='root',pw='')
+			msg = 'master'
 		elif text == '#help#':
 			msg = 'is this true'
 		elif text.startswith('#漏洞') and text.endswith('#'): 
